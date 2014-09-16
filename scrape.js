65de24c9febae473c658cf37489ef22a4a75fdd6
Christopher Saunders <topher6345@gmail.com>
2014-09-16 13:10:18 -0700

    Add pg package.

--- scrape.js
+++ scrape.js
@@ -1,5 +1,21 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('periscrape', 'topher',  'Aa12345678',{
+  dialect: 'postgres',
+  port: 5432
+});
+
+sequelize
+  .authenticate()
+  .complete(function(err) {
+    if (!!err) {
+      console.log('Unable to connect to the database:', err)
+    } else {
+      console.log('Connection has been established successfully.')
+    }
+  })
+
 
 var $;
 request('https://news.ycombinator.com', function (error, response, html) {
