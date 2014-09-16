e9c2b2750dd7fed0d6007dee3eac7369742c0a9e
Christopher Saunders <topher6345@gmail.com>
2014-09-16 13:31:02 -0700

    Add link and title to database.

--- scrape.js
+++ scrape.js
@@ -6,25 +6,58 @@ var sequelize = new Sequelize('periscrape', 'topher',  'Aa12345678',{
   port: 5432
 });
 
+
+var Article = sequelize.define('Article', {
+  title: Sequelize.STRING,
+  link: {
+    type: Sequelize.STRING,
+    unique: true
+  }
+}, {
+  tableName: 'articles'
+});
+
+
+sequelize
+  .sync({ force: true })
+  .complete(function(err) {
+     if (!!err) {
+       console.log('An error occurred while creating the table:', err);
+       process.exit(1);
+     } else {
+       console.log('Connected to database.');
+     }
+  });
+
 sequelize
   .authenticate()
   .complete(function(err) {
     if (!!err) {
-      console.log('Unable to connect to the database:', err)
+      console.log('Unable to connect to the database:', err);
+      process.exit(1);
     } else {
-      console.log('Connection has been established successfully.')
+      console.log('Connection to database has been established successfully.');
     }
-  })
+  });
 
 
-var $;
 request('https://news.ycombinator.com', function (error, response, html) {
   if(error) { return 1; }
-  if (response.statusCode != 200) { return 1; }
-  $ = cheerio.load(html);
+  if (response.statusCode != 200) {
+    console.log("Reponse was not success.");
+    process.exit(1);
+  }
+
+  var $ = cheerio.load(html);
+
   $('span.comhead').each(function(i, element){
     var a = $(this).prev();
-    console.log(a.attr('href'));
+    // console.log("Link: " +a.attr('href'));
+    // console.log("Title: " +a.text());
+    Article.create({
+      title: a.text(),
+      link: a.attr('href')
+    });
   });
 });
 
